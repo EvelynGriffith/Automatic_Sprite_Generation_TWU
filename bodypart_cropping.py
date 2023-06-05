31acc80a870c9d476685cd83a7e2ad61efb041ab
EvelynGriffith <griffith01@allegheny.edu>
2023-06-05 14:36:52 -0700

    trying to fix the cropping

--- bodypart_cropping.py
+++ bodypart_cropping.py
@@ -102,19 +102,72 @@ import matplotlib.pyplot as plt
 
 with open(r'C:\Users\gforc\Automatic_Sprite_Generation_TWU\openpifpaf_TWU\docs\bodypart_cropped_image_folder\new_image.png.predictions.jpeg') as image:
 # Generate some sample data 
+    body_kps = [
+    'nose',            # 1
+    'left_eye',        # 2
+    'right_eye',       # 3
+    'left_ear',        # 4
+    'right_ear',       # 5
+    'left_shoulder',   # 6
+    'right_shoulder',  # 7
+    'left_elbow',      # 8
+    'right_elbow',     # 9
+    'left_wrist',      # 10
+    'right_wrist',     # 11
+    'left_hip',        # 12
+    'right_hip',       # 13
+    'left_knee',       # 14
+    'right_knee',      # 15
+    'left_ankle',      # 16
+    'right_ankle', ]   # 17
+
+    foot_kps = [
+        'left_big_toe',    # 18
+        'left_small_toe',  # 19
+        'left_heel',       # 20
+        'right_big_toe',   # 21
+        'right_small_toe', # 22
+        'right_heel', ]    # 23
+
+    face_kps = ['f_' + str(x) for x in range(24, 92)]
+    lefth_kps = ['lh_' + str(x) for x in range(92, 113)]
+    righth_kps = ['rh_' + str(x) for x in range(113, 134)]
+
+    WHOLEBODY_KEYPOINTS = body_kps + foot_kps + face_kps + lefth_kps + righth_kps
+
+    SCALE_FACE = 1.05
+
+    body_pose = np.array([
+        [0.0, 9.3, 2.0],                 # 'nose',            # 1
+        [-0.35 * SCALE_FACE, 9.7, 2.0],  # 'left_eye',        # 2
+        [0.35 * SCALE_FACE, 9.7, 2.0],   # 'right_eye',       # 3
+        [-0.7 * SCALE_FACE, 9.5, 2.0],   # 'left_ear',        # 4
+        [0.7 * SCALE_FACE, 9.5, 2.0],    # 'right_ear',       # 5
+        [-1.4, 8.0, 2.0],                # 'left_shoulder',   # 6
+        [1.4, 8.0, 2.0],                 # 'right_shoulder',  # 7
+        [-1.75 - 0.4, 6.2 + 0.2, 2.0],   # 'left_elbow',      # 8
+        [1.75 + 0.4, 6.2 + 0.2, 2.0],    # 'right_elbow',     # 9
+        [-1.75 - 0.5, 4.2 + 0.5, 2.0],   # 'left_wrist',      # 10
+        [1.75 + 0.5, 4.2 + 0.5, 2.0],    # 'right_wrist',     # 11
+        [-1.26, 4.0, 2.0],               # 'left_hip',        # 12
+        [1.26, 4.0, 2.0],                # 'right_hip',       # 13
+        [-1.4, 2.0, 2.0],                # 'left_knee',       # 14
+        [1.4, 2.0, 2.0],                 # 'right_knee',      # 15
+        [-1.4, 0.0, 2.0],                # 'left_ankle',      # 16
+        [1.4, 0.0, 2.0], ])              # 'right_ankle',     # 17
+    
     p1 = Point((body_pose[10]))
-    # p2 = Point(body_pose[10])
-    points = gpd.GeoSeries([p1])
+    p2 = Point(body_pose[10])
+    points = gpd.GeoSeries([p1,p2])
 
     # Buffer the points using a square cap style
     # Note cap_style: round = 1, flat = 2, square = 3
     buffer = points.buffer(2, cap_style = 3)
 
     # Plot the results
-    fig, ax1 = plt.subplots()
-    buffer.boundary.plot(ax=ax1, color = 'slategrey')
+    test3 = fig, ax1 = plt.subplots()
+    buffer.boundary.plot(ax=ax1, color = 'white')
     points.plot(ax = ax1, color = 'red')
-    print(fig, ax1)
 
 
 # Opens a image in RGB mode
