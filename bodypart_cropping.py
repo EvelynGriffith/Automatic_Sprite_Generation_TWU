f50a99314806b6d3dcb5e37f61dc440f40111964
EvelynGriffith <griffith01@allegheny.edu>
2023-06-05 13:28:28 -0700

    trying to add the code around a pt

--- bodypart_cropping.py
+++ bodypart_cropping.py
@@ -12,6 +12,7 @@ import numpy as np
 import os
 from PIL import Image
 import easygui as eg
+import subprocess
 # from openpifpaf.plugins.coco import CocoDataset as Coco
 # from .wholebody_metric import WholebodyMetric
 # from .constants import (
@@ -43,15 +44,15 @@ body_kps = [
     'left_knee',       # 14
     'right_knee',      # 15
     'left_ankle',      # 16
-    'right_ankle', ]     # 17
+    'right_ankle', ]   # 17
 
 foot_kps = [
     'left_big_toe',    # 18
     'left_small_toe',  # 19
     'left_heel',       # 20
     'right_big_toe',   # 21
-    'right_small_toe',  # 22
-    'right_heel', ]     # 23
+    'right_small_toe', # 22
+    'right_heel', ]    # 23
 
 face_kps = ['f_' + str(x) for x in range(24, 92)]
 lefth_kps = ['lh_' + str(x) for x in range(92, 113)]
@@ -62,23 +63,23 @@ WHOLEBODY_KEYPOINTS = body_kps + foot_kps + face_kps + lefth_kps + righth_kps
 SCALE_FACE = 1.05
 
 body_pose = np.array([
-    [0.0, 9.3, 2.0],    # 'nose',                         # 1
+    [0.0, 9.3, 2.0],                 # 'nose',            # 1
     [-0.35 * SCALE_FACE, 9.7, 2.0],  # 'left_eye',        # 2
     [0.35 * SCALE_FACE, 9.7, 2.0],   # 'right_eye',       # 3
     [-0.7 * SCALE_FACE, 9.5, 2.0],   # 'left_ear',        # 4
     [0.7 * SCALE_FACE, 9.5, 2.0],    # 'right_ear',       # 5
-    [-1.4, 8.0, 2.0],   # 'left_shoulder',                # 6
-    [1.4, 8.0, 2.0],    # 'right_shoulder',               # 7
-    [-1.75 - 0.4, 6.2 + 0.2, 2.0],  # 'left_elbow',      # 8
-    [1.75 + 0.4, 6.2 + 0.2, 2.0],   # 'right_elbow',     # 9
-    [-1.75 - 0.5, 4.2 + 0.5, 2.0],  # 'left_wrist',      # 10
-    [1.75 + 0.5, 4.2 + 0.5, 2.0],   # 'right_wrist',     # 11
-    [-1.26, 4.0, 2.0],  # 'left_hip',        # 12
-    [1.26, 4.0, 2.0],   # 'right_hip',       # 13
-    [-1.4, 2.0, 2.0],   # 'left_knee',       # 14
-    [1.4, 2.0, 2.0],    # 'right_knee',      # 15
-    [-1.4, 0.0, 2.0],   # 'left_ankle',      # 16
-    [1.4, 0.0, 2.0], ])    # 'right_ankle',     # 17
+    [-1.4, 8.0, 2.0],                # 'left_shoulder',   # 6
+    [1.4, 8.0, 2.0],                 # 'right_shoulder',  # 7
+    [-1.75 - 0.4, 6.2 + 0.2, 2.0],   # 'left_elbow',      # 8
+    [1.75 + 0.4, 6.2 + 0.2, 2.0],    # 'right_elbow',     # 9
+    [-1.75 - 0.5, 4.2 + 0.5, 2.0],   # 'left_wrist',      # 10
+    [1.75 + 0.5, 4.2 + 0.5, 2.0],    # 'right_wrist',     # 11
+    [-1.26, 4.0, 2.0],               # 'left_hip',        # 12
+    [1.26, 4.0, 2.0],                # 'right_hip',       # 13
+    [-1.4, 2.0, 2.0],                # 'left_knee',       # 14
+    [1.4, 2.0, 2.0],                 # 'right_knee',      # 15
+    [-1.4, 0.0, 2.0],                # 'left_ankle',      # 16
+    [1.4, 0.0, 2.0], ])              # 'right_ankle',     # 17
 
 """This is trying to make a folder generate automatically so that I have a place to put the cropped images."""
 # Note: It generates the folder properly.
@@ -87,26 +88,54 @@ newpath = r'C:\Users\gforc\Automatic_Sprite_Generation_TWU\openpifpaf_TWU\docs\b
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
-# Opens a image in RGB mode
-im = Image.open(r"C:\Users\gforc\Automatic_Sprite_Generation_TWU\openpifpaf_TWU\docs\jesus_nobk.png")
- 
-# Size of the image in pixels (size of original image)
-# (This is not mandatory)
-width, height = im.size
+"""Adding the bones to the image...maybe :)"""
+command = r"python -m openpifpaf.predict C:\Users\gforc\Automatic_Sprite_Generation_TWU\openpifpaf_TWU\docs\jesus.png --checkpoint shufflenetv2k16-wholebody --line-width=3 --image-output"
+subprocess.run(command, shell=True)
+output_path = eg.filesavebox(msg='Save file to..', default=r'C:\Users\gforc\Automatic_Sprite_Generation_TWU\openpifpaf_TWU\docs\bodypart_cropped_image_folder/image_with_bones.png')
 
-# Setting the points for cropped image
-# left = [-1.75 - 0.5, 4.2 + 0.5, 2.0] #<-- attempting to use bodypt recognition to identify cropping path.
-#TODO: You may have to figure out how to get the general area of the body part that you're trying to crop based on these coordinates
-# how could you do this mathematically speaking???
-left = 100
-top = height / 4
-right = 164
-bottom = 3 * height / 4
+# command.save(output_path)
+
+""" Attempting to generate a square around the point that we want to crop."""
+import geopandas as gpd
+from shapely.geometry import Point
+import matplotlib.pyplot as plt
+
+# Generate some sample data 
+p1 = Point((body_pose[10]))
+# p2 = Point(body_pose[10])
+points = gpd.GeoSeries([p1])
+
+# Buffer the points using a square cap style
+# Note cap_style: round = 1, flat = 2, square = 3
+buffer = points.buffer(2, cap_style = 3)
+
+# Plot the results
+fig, ax1 = plt.subplots()
+buffer.boundary.plot(ax=ax1, color = 'slategrey')
+points.plot(ax = ax1, color = 'red')
+
+
+# Opens a image in RGB mode
+# #TODO: figure out how to make this image anything that can be picked out
+image = Image.open(r'C:\Users\gforc\Automatic_Sprite_Generation_TWU\openpifpaf_TWU\docs\jesus.png')
+ 
+# # Size of the image in pixels (size of original image)
+# # (This is not mandatory)
+# width, height = image.size
+
+# # Setting the points for cropped image
+# # left = [-1.75 - 0.5, 4.2 + 0.5, 2.0] #<-- attempting to use bodypt recognition to identify cropping path.
+# #TODO: You may have to figure out how to get the general area of the body part that you're trying to crop based on these coordinates
+# # how could you do this mathematically speaking???
+# left = 250
+# top = height / 2
+# right = 500
+# bottom = 3 * height / 2
  
 # Cropped image of above dimension
 output_path = eg.filesavebox(msg='Save file to..', default=r'C:\Users\gforc\Automatic_Sprite_Generation_TWU\openpifpaf_TWU\docs\bodypart_cropped_image_folder\new_image.png')
 # (It will not change original image)
-im1 = im.crop((left, top, right, bottom))
-# Shows the image in image viewer
-im1.show()
-im1.save(output_path)
+# im1 = image.crop((left, top, right, bottom))
+# Shows the image in image viewe
+image.show()
+image.save(output_path)
